dd60dd4cfe2c8253776dd184673bf94c358a5542
ekaterinasavcuk <heeykat@gmail.com>
2024-05-30 17:20:52 +0200

    3. Каркас шахматной доски [#792 #98201]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -21,6 +21,16 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
+        for (Figure figure : figures) {
+            if (figure != null) {
+                for (Cell step : steps) {
+                    if (figure.position().equals(step)) {
+                        throw new OccupiedCellException(
+                                String.format("The cell in the figure's path is busy %s", step));
+                    }
+                }
+            }
+        }
         return true;
     }
 
